103f174bbfdc87f25a566473ec5c611496b9c480
Amine5966 <m.allaf@chronodiali.ma>
2025-02-10 10:20:55 +0100

    auth and sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Bricolage_Grotesque } from "next/font/google"
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const bricolage_Grotesque = Bricolage_Grotesque({
   subsets: ["latin"],
